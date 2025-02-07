610730d28af3670e071cea83e9c8f388d7f7f10f
Wilmar Bustos <wilmardaniel25@gmail.com>
2025-02-07 11:27:27 -0500

    feat: modify supported locales file

--- lib/src/csv_parser.dart
+++ lib/src/csv_parser.dart
@@ -7,17 +7,24 @@ class CSVParser {
   final String strings;
   final List<List<dynamic>> lines;
 
-  CSVParser(this.strings, {this.fieldDelimiter = ','}) : lines = CsvToListConverter().convert(strings, fieldDelimiter: fieldDelimiter);
+  CSVParser(this.strings, {this.fieldDelimiter = ','})
+      : lines = CsvToListConverter()
+            .convert(strings, fieldDelimiter: fieldDelimiter);
 
   String getSupportedLocales() {
-    final locales = lines.first.sublist(1, lines.first.length).map((e) {
-      final languages = e.toString().split('_');
-      return "Locale('${languages[0]}', '${languages[1]}')";
+    final locales = lines.first.sublist(1).map((e) {
+      final parts = e.toString().split('_');
+      final languageCode = parts[0];
+      final countryCode = (parts.length > 1) ? "'${parts[1]}'" : 'null';
+
+      return "Locale('$languageCode', $countryCode)";
     }).toList();
+
     return 'static const supportedLocales = [\n${locales.join(',\n')}\n];';
   }
 
-  String generateTranslationUsages(List<String?> preservedKeywords, bool immediateTranslationEnabled) {
+  String generateTranslationUsages(
+      List<String?> preservedKeywords, bool immediateTranslationEnabled) {
     final List<Item> items = [];
 
     lines.getRange(1, lines.length).forEach((e) {
@@ -120,7 +127,8 @@ class CSVParser {
   }
 
   /// store key to List and ignore preserved-keyword-key
-  void _checkAndAddItem(List<String?> preservedKeywords, List<Item> itemList, Item item) {
+  void _checkAndAddItem(
+      List<String?> preservedKeywords, List<Item> itemList, Item item) {
     final keyParts = item.key.split('.');
 
     final isPlural = _checkIsPluralKey(keyParts);
@@ -137,7 +145,8 @@ class CSVParser {
         );
         continue;
       }
-      if (index == keyParts.length - 1 && preservedKeywords.contains(keyParts[index])) {
+      if (index == keyParts.length - 1 &&
+          preservedKeywords.contains(keyParts[index])) {
         continue;
       }
       _addNewItemToList(
@@ -152,7 +161,8 @@ class CSVParser {
   }
 
   void _addNewItemToList(List<Item> pairList, Item pair) {
-    final isExist = pairList.where((element) => element.key == pair.key).isNotEmpty;
+    final isExist =
+        pairList.where((element) => element.key == pair.key).isNotEmpty;
     if (!isExist) {
       pairList.add(pair);
     }
@@ -170,9 +180,11 @@ class CSVParser {
     return pluralKeywords.contains(keyParts.last);
   }
 
-  String _capitalize(String str) => '${str[0].toUpperCase()}${str.substring(1)}';
+  String _capitalize(String str) =>
+      '${str[0].toUpperCase()}${str.substring(1)}';
 
   String _normalize(String str) => '${str[0].toLowerCase()}${str.substring(1)}';
 
-  String _joinKey(List<String> keys) => _normalize(keys.map((e) => _capitalize(e)).toList().join());
+  String _joinKey(List<String> keys) =>
+      _normalize(keys.map((e) => _capitalize(e)).toList().join());
 }
